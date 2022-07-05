e002874965ccc3b205b8ca7a89cc7a3515beab01
Arthur <trd.arthurmedellin@gmail.com>
2022-07-05 14:39:38 +0800

    changeS

--- api/routes/store_routes.js
+++ api/routes/store_routes.js
@@ -13,6 +13,8 @@ const router = express.Router();
 // router.get('/:id', ConvertedUrl.getByIdConvert);
 
 
+// https://www.aslamdoctor.com/simple-crud-app-using-express-nuxt-js-using-servermiddleware-part-1/
+
 
 
 module.exports = router;
